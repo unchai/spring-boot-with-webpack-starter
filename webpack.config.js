89ac51e0769e1c718762ec5d14718e9f30d73f29
unchai <dotoli21@gmail.com>
2017-05-29 18:55:08 +0900

    Use webpack-manifest-replace-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,10 @@
 const path = require('path');
-const glob = require('glob');
-const fs = require('graceful-fs');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ManifestReplacePlugin = require('webpack-manifest-replace-plugin');
 const commonConfig = require('./webpack.config.common');
-
-function ManifestReplacePlugin(options) {
-  this.pluginOptions = options;
-}
-
-ManifestReplacePlugin.prototype.apply = function (compiler) {
-  var pluginOptions = this.pluginOptions;
-
-  compiler.plugin('done', function () {
-    var manifest = require(path.join(this.options.output.path, pluginOptions.manifestFilename));
-
-    glob(path.join(pluginOptions.basedir, pluginOptions.src), (err, files) => {
-      files.forEach((file) => {
-        fs.readFile(file, 'utf8', function (err, data) {
-          if (err) return console.log(err);
-
-          var result = data;
-          for (var prop in manifest) {
-            result = result.replace(new RegExp(prop, 'gm'), manifest[prop]);
-          }
-
-          fs.writeFile(file, result, 'utf8', function (err) {
-            if (err) return console.log(err);
-          });
-        });
-      });
-    });
-  });
-};
 
 module.exports = webpackMerge(commonConfig, {
   output: {
@@ -58,10 +28,6 @@ module.exports = webpackMerge(commonConfig, {
       },
       comments: false
     }),
-    new ManifestPlugin({
-      fileName: 'rev-manifest.json',
-      basePath: '/static/bundle/'
-    }),
     new CopyWebpackPlugin([
       {
         context: path.resolve(__dirname, 'src/main/webapp/'),
@@ -69,6 +35,10 @@ module.exports = webpackMerge(commonConfig, {
         to: path.resolve(__dirname, 'target/prepare')
       }
     ]),
+    new ManifestPlugin({
+      fileName: 'rev-manifest.json',
+      basePath: '/static/bundle/'
+    }),
     new ManifestReplacePlugin({
       basedir: path.resolve(__dirname, 'target/prepare'),
       src: '**/*.+(jsp|html|htm)',
