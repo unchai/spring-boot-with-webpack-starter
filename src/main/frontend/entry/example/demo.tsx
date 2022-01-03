33625bea90ee2abd51f8f1b141bcb42ec01869bd
Jaehyeon Nam <unchai@protonmail.com>
2022-01-03 16:07:29 +0900

    fix: eslint and prettier config (#24)

---
+++ src/main/frontend/entry/example/demo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../../css/styleA.css';
+import '../../css/styleB.css';
+
+ReactDOM.render(<h1>Hello, world! I&apos;m example/demo page.</h1>, document.getElementById('root'));
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,78 +10,75 @@ const TerserPlugin = require('terser-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = (env = { debug: false, analyze: false }) => {
-  const { debug, analyze } = env;
-  const webpackConfig = merge(commonConfig, {
-    mode: 'production',
-    devtool: debug ? 'cheap-module-source-map' : false,
-    output: {
-      filename: '[name]-[chunkhash].js',
-      path: path.join(__dirname, 'build/prepare/BOOT-INF/classes/static/bundle'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            { loader: 'css-loader', options: { sourceMap: debug }}
-          ],
+    const { debug, analyze } = env;
+    const webpackConfig = merge(commonConfig, {
+        mode: 'production',
+        devtool: debug ? 'cheap-module-source-map' : false,
+        output: {
+            filename: '[name]-[chunkhash].js',
+            path: path.join(__dirname, 'build/prepare/BOOT-INF/classes/static/bundle'),
         },
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules|src\/main\/frontend\/vendor/,
-          use: 'babel-loader',
-        },
-      ],
-    },
-    performance: {
-      hints: false,
-    },
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          parallel: true,
-          terserOptions: {
-            compress: {
-              drop_console: !debug,
-              drop_debugger: !debug,
-              unused: true,
-              dead_code: true,
-            },
-            output: {
-              beautify: false,
-              comments: false,
-            },
-            warnings: false,
-          },
-        }),
-        new CssMinimizerPlugin({
-          minimizerOptions: {
-            preset: [
-              'default',
-              {
-                discardComments: {
-                  removeAll: true,
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, { loader: 'css-loader', options: { sourceMap: debug } }],
                 },
-              },
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules|src\/main\/frontend\/vendor/,
+                    use: 'babel-loader',
+                },
             ],
-          },
-        }),
-      ],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({ filename: '[contenthash].css' }),
-      new ManifestReplacePlugin({
-        include: path.resolve(__dirname, 'src/main/resources/templates'),
-        test: /\.(jsp|html|htm)$/,
-        outputDir: path.resolve(__dirname, 'build/prepare/BOOT-INF/classes/templates'),
-      }),
-    ],
-  });
+        },
+        performance: {
+            hints: false,
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    terserOptions: {
+                        compress: {
+                            drop_console: !debug,
+                            drop_debugger: !debug,
+                            unused: true,
+                            dead_code: true,
+                        },
+                        output: {
+                            beautify: false,
+                            comments: false,
+                        },
+                        warnings: false,
+                    },
+                }),
+                new CssMinimizerPlugin({
+                    minimizerOptions: {
+                        preset: [
+                            'default',
+                            {
+                                discardComments: {
+                                    removeAll: true,
+                                },
+                            },
+                        ],
+                    },
+                }),
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({ filename: '[contenthash].css' }),
+            new ManifestReplacePlugin({
+                include: path.resolve(__dirname, 'src/main/resources/templates'),
+                test: /\.(jsp|html|htm)$/,
+                outputDir: path.resolve(__dirname, 'build/prepare/BOOT-INF/classes/templates'),
+            }),
+        ],
+    });
 
-  if (analyze) {
-    webpackConfig.plugins.push(new (require('webpack-bundle-analyzer')).BundleAnalyzerPlugin());
-  }
+    if (analyze) {
+        webpackConfig.plugins.push(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)());
+    }
 
-  return webpackConfig;
+    return webpackConfig;
 };
